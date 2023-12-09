9b6795aec13e2c76a652cee3b3838bc2a8da8afa
Vinicius Rodrigues <rodrigues.vini.2004@gmail.com>
2023-12-09 20:24:26 -0300

    update

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const formulario = document.querySelector(".formulario");
 const buttonSubmit = document.querySelector(".btnSubmit");
 const salarioResult = document.querySelector(".salarioResult");
 const abonoResult = document.querySelector(".abonoResult");
@@ -12,124 +11,148 @@ const descontosResult = document.querySelector(".descontosResult");
 const liquidoResult = document.querySelector(".liquidoResult");
 
 class Calcula {
-    constructor(salario, dias, medias, abono, decimo) {
-        this.salario = salario;
-        this.dias = dias;
+    constructor(salarioBase, diasDeFerias, medias, abonoCheck, decimoCheck) {
+        this.salarioBase = salarioBase;
+        this.diasDeFerias = diasDeFerias;
         this.medias = medias;
-        this.abono = abono;
-        this.decimo = decimo;
-        this.tercoProporcial = 0;
-        this.tercoAbono = 0;
+        this.abonoCheck = abonoCheck;
+        this.decimoCheck = decimoCheck;
+
+        this.salarioProporcional = 0;
+        this.tercoSalarioProporcial = 0;
+
+        this.abonoPecuario = 0;
+        this.tercoAbonoPecuario = 0;
+
+        this.parcelaDecimoTerceiro = 0;
+
         this.descontoInss = 0;
         this.descontoIr = 0;
+        
+        this.proventosTotais = 0
+        this.descontosTotais = 0
+        this.liquidoTotal = 0
 
-        console.log(this.decimo)
+        this.main(this.salarioBase, this.diasDeFerias, this.medias, this.abonoCheck, this.decimoCheck);
 
-        this.abono === true ? this.feriasComAbono() : this.feriasSemAbono();
     }
 
-    feriasSemAbono() {
-        const salarioProporcial = (this.salario / 30) * this.dias;
-        this.tercoProporcial = (salarioProporcial + this.medias) / 3;
-
-        const proventos = salarioProporcial + this.tercoProporcial;
-
-        if(this.decimo === true){
-            this.parcelaDecimoTerceiro(this.salario, proventos);
-            return; 
-        }
-
-        this.descontosBase(proventos);
+    CalculaFerias(salarioBase, diasDeFerias) {
+        this.salarioProporcional = (salarioBase / 30) * diasDeFerias;
     }
 
-    feriasComAbono() {
-        const salarioProporcial = (this.salario / 30) * this.dias;
-        this.tercoProporcial = (salarioProporcial + this.medias) / 3;
-
-        const abono = (salarioProporcial / 30) * 10;
-        this.tercoAbono = abono / 3;
-
-        const proventos = salarioProporcial + this.tercoProporcial + abono + this.tercoAbono;
-
-        if(this.decimo === true){
-            this.parcelaDecimoTerceiro(this.salario, proventos);
-            return; 
-        }
-
-        this.descontosBase(proventos);
+    CalculaTercoProporcinal(salarioBase, medias){
+        this.tercoSalarioProporcial = (salarioBase + medias) / 3;
     }
 
-    parcelaDecimoTerceiro(salario, proventos){
-        const decimo = salario / 2;
-
-        this.descontosBase(proventos + decimo);
+    CalculaAbonoPecuario(salarioBase) {
+        this.abonoPecuario = (salarioBase / 30) * 10;
     }
 
-    descontosBase(proventos) {
-        this.descontoInss = this.calcularDescontoInss(proventos);
-        this.descontoIr = this.calcularDescontoIr(proventos);
-
-        const descotosTotais = this.descontoInss + this.descontoIr;
-
-        const liquido = proventos - descotosTotais;
-
-        this.exibir(proventos, descotosTotais, liquido);
+    CalcularTercoAbonoPecuario(salarioBase){
+        this.tercoAbonoPecuario = ((salarioBase / 30) * 10) / 3;
     }
 
-    calcularDescontoInss(salario) {
-        let descontarInss = 0;
-
-        if (salario > 0 && salario < 1320) {
-            descontarInss = 0.075; // 7.5%
-            return Math.min(salario * descontarInss, 99);
-        } else if (salario >= 1320.01 && salario < 2571.29) {
-            descontarInss = 0.09; // 9.0%
-            return Math.min(salario * descontarInss, 112.62);
-        } else if (salario >= 2571.30 && salario < 3856.94) {
-            descontarInss = 0.12; // 12.0%
-            return Math.min(salario * descontarInss, 154.28);
-        } else if (salario >= 3856.95 && salario < 7507.29) {
-            descontarInss = 0.14; // 14.0%
-            return Math.min(salario * descontarInss, 511.05);
-        } else if (salario > 7507.29) {
-            descontarInss = 876.94
-            return descontarInss;
-        }
-
-        return 0;
+    CalculaParcelaDecimoTerceiro(salarioBase){
+       this.parcelaDecimoTerceiro = salarioBase / 2; 
     }
 
-    calcularDescontoIr(salario) {
-        let descontarIr = 0;
+    main(salarioBase, diasDeFerias, medias, abonoCheck, decimoCheck){
 
-        if (salario >= 1903.99 && salario < 2826.65) {
-            descontarIr = 0.075; // 7.5%
-            return Math.min(salario * descontarIr, 158.40);
-        } else if (salario >= 2826.66 && salario < 3751.05) {
-            descontarIr = 0.15; // 15%
-            return Math.min(salario * descontarIr, 370.40);
-        } else if (salario >= 3751.06 && salario < 4664.68) {
-            descontarIr = 0.225; // 22,50%
-            return Math.min(salario * descontarIr, 651.73);
-        } else if (salario >= 4664.69) {
-            descontarIr = 0.275; // 27,50%
-            return Math.min(salario * descontarIr, 884.96);
-        }
+        this.CalculaFerias(salarioBase, diasDeFerias);
+        this.CalculaTercoProporcinal(salarioBase, medias);
 
-        return 0;
+        if(abonoCheck) this.CalculaAbonoPecuario(salarioBase);
+        if(abonoCheck) this.CalcularTercoAbonoPecuario(salarioBase);
+        if(decimoCheck) this.CalculaParcelaDecimoTerceiro(salarioBase);
+
+        this.calcularDescontoInss(salarioBase);
+        this.calcularDescontoIr(salarioBase);
+
+        this.CalculaGerais();
+
+        this.exibirNaTela(abonoCheck, decimoCheck);
+
     }
 
-    exibir(proventos, descontos, liquido) {
-        proventosResult.innerText = `Proventos: ${proventos.toFixed(2)}`
-        descontosResult.innerText = `Descontos: ${descontos.toFixed(2)}`
-        liquidoResult.innerText = `Liquido: ${liquido.toFixed(2)}`
-        salarioResult.innerText = `R$ ${this.salario}`;
-        abonoResult.innerText = this.abono ? `R$ ${(this.tercoAbono * 3).toFixed(2)}` : '0.00'; // Exibe o valor do abono se for verdadeiro, senão exibe 0.00
-        umtercoResult.innerText = `R$ ${this.tercoProporcial.toFixed(2)}`;
-        umTercoAbonoResult.innerText = `R$ ${this.tercoAbono.toFixed(2)}`;
+    CalculaGerais(){
+
+        this.proventosTotais = 
+            this.salarioProporcional +
+            this.tercoSalarioProporcial +
+            this.abonoPecuario +
+            this.tercoAbonoPecuario +
+            this.parcelaDecimoTerceiro +
+            this.medias;
+
+        this.descontosTotais = this.descontoInss + this.descontoIr;
+
+        this.liquidoTotal = this.proventosTotais - this.descontosTotais;
+    }
+
+    exibirNaTela(abonoCheck, decimoCheck) {
+        salarioResult.innerText = `R$ ${this.salarioProporcional.toFixed(2)}`;
+        umtercoResult.innerText = `R$ ${this.tercoSalarioProporcial.toFixed(2)}`;
+
+        abonoResult.innerText = abonoCheck ? `R$ ${this.abonoPecuario.toFixed(2)}` : '0.00';
+        umTercoAbonoResult.innerText = abonoCheck ? `R$ ${this.tercoAbonoPecuario.toFixed(2)}` : '0.00';
+
+        // colocar para exibir a parcela tambem!! usar o decimoCheck para fazer isso igual o abono
+
         mediasResult.innerText = `R$ ${this.medias.toFixed(2)}`;
+
         inssResult.innerText = `R$ ${this.descontoInss.toFixed(2)}`;
         irResult.innerText = `R$ ${this.descontoIr.toFixed(2)}`;
+
+        proventosResult.innerText = `Proventos: ${this.proventosTotais.toFixed(2)}`;
+        descontosResult.innerText = `Descontos: ${this.descontosTotais.toFixed(2)}`;
+        liquidoResult.innerText = `Liquido: ${this.liquidoTotal.toFixed(2)}`;
+    }
+
+    calcularDescontoInss(salarioBase) {
+        let inss = 0;
+
+        if (salarioBase > 0 && salarioBase < 1320) {
+            inss = 0.075; // 7.5%
+            this.descontoInss = Math.min(salarioBase * inss, 99);
+        } else if (salarioBase >= 1320.01 && salarioBase < 2571.29) {
+            inss = 0.09; // 9.0%
+            this.descontoInss = Math.min(salarioBase * inss, 112.62);
+        } else if (salarioBase >= 2571.30 && salarioBase < 3856.94) {
+            inss = 0.12; // 12.0%
+            this.descontoInss = Math.min(salarioBase * inss, 154.28);
+        } else if (salarioBase >= 3856.95 && salarioBase < 7507.29) {
+            inss = 0.14; // 14.0%
+            this.descontoInss = Math.min(salarioBase * inss, 511.05);
+        } else if (salarioBase > 7507.29) {
+            inss = 876.94
+            this.descontoInss = inss;
+        } else {
+            this.descontoInss = 0;
+        }
+
+    }
+
+    calcularDescontoIr(salarioBase) {
+        let ir = 0;
+
+        if (salarioBase >= 1903.99 && salarioBase < 2826.65) {
+            ir = 0.075; // 7.5%
+            this.descontoIr = Math.min(salarioBase * ir, 158.40);
+        } else if (salarioBase >= 2826.66 && salarioBase < 3751.05) {
+            ir = 0.15; // 15%
+            this.descontoIr = Math.min(salarioBase * ir, 370.40);
+        } else if (salarioBase >= 3751.06 && salarioBase < 4664.68) {
+            ir = 0.225; // 22,50%
+            this.descontoIr = Math.min(salarioBase * ir, 651.73);
+        } else if (salarioBase >= 4664.69) {
+            ir = 0.275; // 27,50%
+            this.descontoIr = Math.min(salarioBase * ir, 884.96);
+            return;
+        } else {
+            this.descontoIr = 0;
+        }
+
     }
 }
 
@@ -146,4 +169,5 @@ buttonSubmit.addEventListener("click", e => {
 });
 
 
-//ta somando tudo errado
+// mudar a forma que esta sendo feitos os descontos, para arrays
+// quando tiver abono os dias de ferias nao podem ser superior a 20 dias
